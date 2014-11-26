1dd520e84075d08964f412c1c64a1d107526d62e
coreygriggs <griggscorey@gmail.com>
2014-11-26 07:34:08 -0600

    added a couple of tests

--- coreygriggs_blog/blog/tests.py
+++ coreygriggs_blog/blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Post
+
 
 # Create your tests here.
+class CreatePostTest(TestCase):
+
+	def setUp(self):
+		Post.objects.create(title='testtesttest', slug='test_slug', description='blablablablbablah',
+		            content='this is some test content')
+
+	def test_create_test_adds_time(self):
+		post = Post.objects.get(title='testtesttest', slug='test_slug', description='blablablablbablah',
+		            content='this is some test content')
+		self.assertIsNotNone(post.created)
+
+	def test_create_test_adds_published(self):
+		post = Post.objects.get(title='testtesttest', slug='test_slug', description='blablablablbablah',
+		            content='this is some test content')
+		self.assertIsNotNone(post.published)
